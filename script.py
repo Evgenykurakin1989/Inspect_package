40222b5a061426fdc8ea9498797560150fb01fb7
Evgenykurakin1989 <evgenykurakin19891031@gmail.com>
2019-09-02 13:08:16 +0300

    change code

--- script.py
+++ script.py
@@ -3,38 +3,62 @@ import holidays
 import sys, inspect
 from pprint import pprint
 
-# get all classes from holidays package
-clsmembers = inspect.getmembers(sys.modules['holidays'], inspect.isclass)
 
-inspect_data = []
+def check_classes():
+    """
+    Check all classes inside of package and return array
+    :return: array
+    """
 
-for cls in clsmembers:
+    # get all classes from holidays package
+    cls_members = inspect.getmembers(sys.modules['holidays'], inspect.isclass)
 
-    # check if the first letter of class is Uppercase.
-    if cls[0][0].isupper() and not cls[0] == 'HolidayBase':
-        is_provinces = False
-        is_states = False
-        # create new object with class
-        obj = cls[1]()
+    inspect_data = []
 
-        # check if the object has PROVICES or not
-        try:
-            if obj.PROVINCES:
-                is_provinces = True
-        except:
-            pass
+    for cls in cls_members:
 
-        try:
-            if obj.STATES:
-                is_states = True
-        except:
-            pass
+        # check if the first letter of class is Uppercase.
+        if cls[0][0].isupper() and not cls[0] == 'HolidayBase':
+            is_provinces = False
+            is_states = False
+            # create new object with class
+            obj = cls[1]()
 
-        inspect_data.append({
-            "name": cls[0],
-            "code": obj.country,
-            "states": is_states,
-            "provinces": is_provinces
-        })
+            # check if the object has PROVICES or not
+            try:
+                if obj.PROVINCES:
+                    is_provinces = True
+            except:
+                pass
 
-pprint(inspect_data)
+            try:
+                if obj.STATES:
+                    is_states = True
+            except:
+                pass
+
+            """
+            if not obj.__class__.__base__.__name__ == 'HolidayBase':
+                country_name = obj.__class__.__base__.__name__
+            else:
+                country_name = cls[0]
+            inspect_data.append({
+                "name": country_name,
+                "code": obj.country,
+                "states": is_states,
+                "provinces": is_provinces
+            })
+            """
+
+            inspect_data.append({
+                "name": cls[0],
+                "code": obj.country,
+                "states": is_states,
+                "provinces": is_provinces
+            })
+
+    return inspect_data
+
+
+data = check_classes()
+pprint(data)
